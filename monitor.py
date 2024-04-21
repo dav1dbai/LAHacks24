fe9db1df0a6c5cb9e14c659f759e17c351355e7b
Weihe Zheng <wayzheng@usc.edu>
2024-04-20 23:41:04 -0700

    Update monitor.py, cannot run

--- monitor.py
+++ monitor.py
@@ -13,7 +13,8 @@ import PIL.Image
 import time
 import pytz
 
-#nee to first run quickstart and then monitor would work. check why this happens.
+#need to first run quickstart and then monitor would work. check why this happens.
+#need to do with the frontend
 
 #way apr 21
 #now it can takes screenshoot, and 
@@ -71,6 +72,10 @@ def is_event_happening_now(service):
         if event_start_time < local_now:
             return True   
     return False  # No event is happening now or comparison fails
+  
+def contains_no(phrase):
+    # \b around 'no' defines a word boundary, ensuring 'no' is a complete word
+    return bool(re.search(r'\bno\b', phrase, re.IGNORECASE))
 
 def check(service):
     # Take a screenshot
@@ -78,13 +83,13 @@ def check(service):
 
     # Get current event description
     event_description = get_current_event_description(service)
-    prompt = f"Does this screenshot match the event description: '{event_description}'?"
+    prompt = f"Does this screenshot match the event description: '{event_description}'? Give Yes or No"
     img = PIL.Image.open(screenshot_path)
     
     # ask gemini if this is the same event
     model = genai.GenerativeModel('gemini-pro-vision')
     response = model.generate_content([prompt,img])
-    print(response.text)
+    return (contains_no(phrase))
     #https://ai.google.dev/gemini-api/docs/get-started/python 
 
 
@@ -96,10 +101,16 @@ def main():
 
     #about to write some logics in checking servise 
     #1 when there is an event going on, during the strat and the end time, check every 5 minutes
-    while (is_event_happening_now(service)):
-        print(is_event_happening_now(service))
-        check(service)
-        time.sleep(300) #check every 5 minuts
+    while True:
+      if (is_event_happening_now(service)):
+          time.sleep(300)
+          #print(is_event_happening_now(service))
+          if (not check(service)):
+            #post a thing to change the frontend
+            #TO DO
+            else:
+              #happy Cappy
+          time.sleep(300) #check every 5 minuts
 
 
 
